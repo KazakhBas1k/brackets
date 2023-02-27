0f667d6a7fb19cc153cefd27935ada8d6c57ecca
KazakhBas1k <darhan.balmukhambet@gmail.com>
2023-02-28 02:33:57 +0600

    feat: brakets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brackets = str;
+  for (let i = 0; i < Math.trunc(str.length / 2); i++) {
+    bracketsConfig.forEach((item) => {
+      brackets = brackets.replace(`${item[0]}${item[1]}`, "");
+    });
+  }
+  return brackets.length == 0;
+};
